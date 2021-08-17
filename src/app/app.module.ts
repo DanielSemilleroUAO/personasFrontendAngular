2619b2964c2d4dc9f1a8da2a6813eab48525d2bb
Daniel Delgado Rodríguez <danieldelgado_2106@hotmail.com>
2021-08-17 16:37:12 -0500

    Se crea proyecto y componentes para el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { ErrorComponent } from './error/error.component';
+import { ListPersonsComponent } from './list-persons/list-persons.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MenuComponent,
+    ErrorComponent,
+    ListPersonsComponent,
+    LoginComponent,
+    LogoutComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule,
